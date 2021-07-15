d129971e948657bc63df0893e1190daa4744b2ab
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-07-14 22:27:54 -0700

    Schema updates, add readme notes on scripts

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 To run schema: 
 -- log into postgres and run '\i database/schema.sql'. This will create the database and tables.
+-- run '\i database/load_data.sql'. This will load the data. 
---
+++ database/load_data.sql
@@ -0,0 +1,15 @@
+-- Load data into tables
+\COPY Product_Info(id,name,slogan,description,category,default_price) FROM '/Users/pscheutzow/Desktop/Data/product.csv' DELIMITER ',' CSV HEADER;
+\COPY Related(id,productId,related_productId) FROM '/Users/pscheutzow/Desktop/Data/related.csv' DELIMITER ',' CSV HEADER;
+\COPY Styles(id,productId,name,sale_price,original_price,default_style) FROM '/Users/pscheutzow/Desktop/Data/styles.csv' DELIMITER ',' CSV HEADER;
+\COPY Features(id,productId,feature,value) FROM '/Users/pscheutzow/Desktop/Data/features.csv' DELIMITER ',' CSV HEADER;
+\COPY Photos(id,styleId,url,thumbnail_url) FROM '/Users/pscheutzow/Desktop/Data/photos.csv' DELIMITER ',' CSV HEADER;
+\COPY SKUS(id,styleId,size,quantity) FROM '/Users/pscheutzow/Desktop/Data/skus.csv' DELIMITER ',' CSV HEADER;
+
+-- Check first row of data
+SELECT * FROM Product_Info WHERE id = 1;
+SELECT * FROM Related WHERE id = 1;
+SELECT * FROM Styles WHERE id = 1;
+SELECT * FROM Features WHERE id = 1;
+SELECT * FROM Photos WHERE id = 1;
+SELECT * FROM SKUS WHERE id = 1;
