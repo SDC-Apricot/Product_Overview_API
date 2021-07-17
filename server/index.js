458a332b9f83ce7bfa6b4af70251fb8eed9c935b
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-07-16 18:01:24 -0700

    Add file for database connection, setting up routes in server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const db = require('../database/connect.js');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World');
+});
+
+// Retrieves the list of products.
+app.get('/products', (req, res) => {
+  db.client.query('SELECT * FROM Product_Info where id = 1', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result.rows);
+    }
+  })
+});
+
+// Returns all product level information for a specified product id.
+app.get('/products/:product_id', (req, res) => {
+  
+});
+
+// Returns the all styles available for the given product.
+app.get('/products/:product_id/styles', (req, res) => {
+  
+});
+
+// Returns the id's of products related to the product specified.
+app.get('/products/:/products/:product_id/related', (req, res) => {
+  
 });
 
 app.listen(port, () => {
