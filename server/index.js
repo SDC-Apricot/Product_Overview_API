09df74684d16ba895d35046579890191b86245b6
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-07-19 20:41:39 -0700

    get /products/:product_id/styles returns nested object data

--- server/index.js
+++ server/index.js
@@ -51,28 +51,6 @@ app.get('/products/:product_id', (req, res) => {
   })
 });
 
-// SELECT 
-// p.productId AS productId, 
-// p.name AS productName,
-// s.id AS styleId, 
-// s.name AS styleName, 
-// s.original_price AS styleOriginalPrice, 
-// s.default_style AS styleDefault, 
-// s.sale_price AS styleSalePrice, 
-// ph.styleId AS photoStyleId,
-// ph.url AS photoURL,
-// ph.thumbnail_url AS photoThumbURL, 
-// sk.size AS skuSize,
-// sk.quantity AS skuQuantity
-// FROM Product_Info p
-// LEFT OUTER JOIN Styles s 
-// ON s.productId = p.productId
-// LEFT OUTER JOIN Photos ph
-// ON ph.styleId = s.id
-// LEFT OUTER JOIN SKUS sk
-// ON sk.styleId = s.id
-// WHERE s.productId = ${product_id}
-
 // Returns the all styles available for the given product.
 app.get('/products/:product_id/styles', (req, res) => {
   let product_id = 1;
@@ -94,11 +72,20 @@ app.get('/products/:product_id/styles', (req, res) => {
                   FROM Photos
                   WHERE Styles.id = photos.styleId
                 ) row_inner
-              ) as photos
+              ) as photos, 
+              (
+                SELECT array_to_json(array_agg(row_to_json(row_skus)))
+                FROM (
+                  SELECT 
+                    *
+                  FROM SKUS
+                  WHERE Styles.id = skus.styleId
+                ) row_skus
+              ) as skus
             FROM Styles
             WHERE Styles.productId = product_info.productId
           ) row
-        ) as styles
+        ) as results
       FROM Product_Info
       WHERE productId = ${product_id}
     ) id
