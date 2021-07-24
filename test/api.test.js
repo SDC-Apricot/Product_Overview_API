a3413f253075530915c79ae0914e1338b9163b74
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-07-24 17:32:18 -0400

    WIP

--- test/api.test.js
+++ test/api.test.js
@@ -1,29 +1,33 @@
 const request = require('supertest');
 const app = require('../server/app.js');
-const {productsMock, productsInfoMock, productStylesMock, relatedProductsMock} = require('./../data/products.js');
 
-afterAll(() => jest.setTimeout(5 * 1000))
+const { Client } = require('pg');
+const axios = require('axios');
+const { response } = require('express');
+const port = 4000;
+require('dotenv').config();
 
-describe('GETs data in json format for all routes', function() {
-  it('response for /products', function(done) {
+
+describe('GETs data in json format for all routes', function () {
+  it('response for /products', function (done) {
     request(app)
       .get('/products')
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
-  it('response for /products/:product_id', function(done) {
+  it('response for /products/:product_id', function (done) {
     request(app)
       .get('/products/:product_id')
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
-  it('response for /products/:product_id/styles', function(done) {
+  it('response for /products/:product_id/styles', function (done) {
     request(app)
       .get('/products/:product_id')
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
-  it('response for /products/:product_id/related', function(done) {
+  it('response for /products/:product_id/related', function (done) {
     request(app)
       .get('/products/:product_id')
       .expect('Content-Type', /json/)
@@ -31,19 +35,21 @@ describe('GETs data in json format for all routes', function() {
   });
 });
 
-describe('Checks data lengths are greater than 0 or 10 where limit is set', function() {
-  it('responds with json', function() {
+describe('Checks data lengths are greater than 0 or 10 where limit is set', function () {
+  it('responds with json', function () {
     return request(app)
       .get('/products')
       .then(response => response.body.length)
       .then(num => num === 10)
+      .catch(err => err)
   });
-  it('responds with json', function() {
+  it('responds with json', function () {
     return request(app)
       .get('/products/:product_id')
       .expect(200)
       .then(response => response.body.length)
       .then(num => num > 0)
+      .catch(err => err)
   });
 });
 
